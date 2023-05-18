fb245e3a1465efeaaac4e9ec10abf3951c986a3c
giovanni <giopftw@gmail.com>
2023-05-18 15:05:13 +0200

    removed unused stuff

--- int-to-hex.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-
-    alfa = '32798 49167 57351 61443 63489 64512 65024 65280 65408 65472'
-    separeted = alfa.split(' ')
-    for i in separeted:
-        print(hex(int(i) + 65536))
